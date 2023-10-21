5e4bc6e7fa84c684519080517e6a96b3b17d0cfb
Vivek <vivekparde2411@gmail.com>
2023-10-21 19:06:42 +0530

    Added server modal

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
+
+interface ModalStore {
+    type: ModalType | null;
+    isOpen: boolean;
+    onOpen: (type: ModalType) => void;
+    onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+    type: null,
+    isOpen: false,
+    onOpen: (type) => set({ isOpen: true, type }),
+    onClose: () => set({ type: null, isOpen: false })
+}));
