6bd8a30cf57348a672226eb77e5d482268aff439
Vivek <vivekparde2411@gmail.com>
2023-10-22 21:38:05 +0530

    Added edit server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -52,13 +52,14 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                     <DropdownMenuItem
                         onClick={() => onOpen("invite", { server })}
                         className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
-                    >
+                        >
                         Invite People
                         <UserPlus className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("editServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server Settings
