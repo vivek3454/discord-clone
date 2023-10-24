aed612b624177e372e9a304c37a656352a474e2a
Vivek <vivekparde2411@gmail.com>
2023-10-24 23:34:45 +0530

    Added delete server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -7,7 +7,7 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
     try {
         const profile = await currentProfile();
         const { name, imageUrl } = await req.json();
-        
+
         if (!profile) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
@@ -26,6 +26,31 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
 
     } catch (error) {
         console.log("[SERVER_ID_PATCH]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            }
+        });
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE]", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
