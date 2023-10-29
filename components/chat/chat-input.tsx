366d5436d33f5264b90b02610afdb00e1beccf5b
Vivek <vivekparde2411@gmail.com>
2023-10-29 16:37:17 +0530

    Added message api

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -28,7 +28,12 @@ const formSchema = z.object({
     content: z.string().min(1),
 });
 
-const ChatInput = ({ apiUrl, query, name, type, }: ChatInputProps) => {
+const ChatInput = ({
+    apiUrl,
+    query,
+    name,
+    type,
+}: ChatInputProps) => {
     const { onOpen } = useModal();
     const router = useRouter();
 
@@ -42,7 +47,19 @@ const ChatInput = ({ apiUrl, query, name, type, }: ChatInputProps) => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query,
+            });
+
+            await axios.post(url, values);
+
+            form.reset();
+            router.refresh();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -57,6 +74,7 @@ const ChatInput = ({ apiUrl, query, name, type, }: ChatInputProps) => {
                                 <div className="relative p-4 pb-6">
                                     <button
                                         type="button"
+                                        onClick={() => onOpen("messageFile", { apiUrl, query })}
                                         className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                                     >
                                         <Plus className="text-white dark:text-[#313338]" />
