20927590e6b1bf956ea76fda1379ac4f043d47bb
Vivek <vivekparde2411@gmail.com>
2023-10-29 18:23:35 +0530

    Added send image and pdf functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,8 +9,9 @@ import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal";
 import LeaveServerModal from "@/components/modals/leave-server-modal";
 import DeleteServerModal from "@/components/modals/delete-server-modal";
-import DeleteChannelModal from "../modals/delete-channel-modal";
-import EditChannelModal from "../modals/edit-channel-modal";
+import DeleteChannelModal from "@/components/modals/delete-channel-modal";
+import EditChannelModal from "@/components/modals/edit-channel-modal";
+import MessageFileModal from "@/components/modals/message-file-modal";
 
 const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -34,6 +35,7 @@ const ModalProvider = () => {
             <DeleteServerModal />
             <DeleteChannelModal />
             <EditChannelModal />
+            <MessageFileModal />
         </>
     );
 };
